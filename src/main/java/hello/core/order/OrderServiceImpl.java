6d45f7d06231ec0528bd62370156857ec095ec1a
Jiwon <wldnjs980409@naver.com>
2023-07-01 01:47:29 +0900

    DIP 위반 변경ing

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
+    // 구체에 의존하지 않고, 인터페이스(추상)에만 의존 (DIP 잘 지킴)
+    // 아무 것도 할당되지 않았으므로 NullPointException 에러 발생
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
