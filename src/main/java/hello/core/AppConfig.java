59579a17dd8939668d60139500f7626bd2c9ed61
Jiwon <wldnjs980409@naver.com>
2023-07-03 18:41:39 +0900

    AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -13,11 +15,19 @@ import hello.core.order.OrderServiceImpl;
 // 관심사의 분리 : 역할과 실행 분리
 
 public class AppConfig {
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());   // 생성자를 통해 생성한 객체 주입
+    public MemberService memberService(){    // 메서드명, 리턴타입을 보면 역할이 드러난다.
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());  // 생성자 주입
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
