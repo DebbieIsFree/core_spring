3a751bdedb16371198ae64a6b54d764ec45ce240
Jiwon <wldnjs980409@naver.com>
2023-07-29 08:55:49 +0900

    싱글톤에서 stateless 필드로 변경

--- src/test/java/hello/singleton/StatelessServiceTest.java
+++ src/test/java/hello/singleton/StatelessServiceTest.java
@@ -6,28 +6,25 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-
-class StatefulServiceTest {
+class StatelessServiceTest {
     @Test
     void statefulServiceSingleton(){ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
-        StatefulService statefulService1 = ac.getBean(StatefulService.class);
-        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+        StatelessService statelessService1 = ac.getBean(StatelessService.class);
+        StatelessService statelessService2 = ac.getBean(StatelessService.class);
 
         // ThreadA : A 사용자가 10,000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statelessService1.order("userA", 10000);
         // ThreadB : B 사용자가 20,000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statelessService2.order("userB", 20000);
 
         // ThreadA : 사용자 A가 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
-
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        System.out.println("price = " + userAPrice);
     }
+
     static class TestConfig {
         @Bean
-        public StatefulService statefulService(){
-            return new StatefulService();
+        public StatelessService statefulService(){
+            return new StatelessService();
         }
     }
 }
