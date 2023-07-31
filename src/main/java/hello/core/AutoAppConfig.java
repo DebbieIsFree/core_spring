1e39ffa48dbe2e39a969d30157de9b3715352e11
Jiwon <wldnjs980409@naver.com>
2023-07-31 13:16:52 +0900

    컴포넌트 스캔 탐색 위치 지정, 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,12 @@ import org.springframework.context.annotation.FilterType;
 // 설정 정보 : Configuration
 @Configuration
 @ComponentScan(
+        // hello.core부터 찾아 들어간다.
+        basePackages = "hello.core",
+
+        // AutoAppConfig가 있는 hello.core 패키지부터 시작한다.
+        basePackageClasses = AutoAppConfig.class,
+
         // 자동으로 스프링 빈으로 등록할 때 제외할 것을 선정
         // @ComponentScan을 사용하면 @Component가 붙은 설정 정보도 자동으로 등록된다. (ex. AppConfig 파일)
         // @Configuration 어노테이션이 붙은 것은 제외
