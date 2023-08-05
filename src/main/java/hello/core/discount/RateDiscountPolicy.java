c0a59c55a2703ae5d35400a64690636117fbeb2b
Jiwon <wldnjs980409@naver.com>
2023-08-06 08:56:38 +0900

    Annotation 직접 만들어 사용하기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,7 +8,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Qualifier("mainDiscountPolicy")
+//@Qualifier("mainDiscountPolicy")     // 문자열 오류가 발생하면 컴파일 타임 오류라서 발견할 수가 없다.
+@MainDiscountPolicy         // 그래서 직접 만든 애노테이션 사용, 이때 문자열 오류가 발생하면 잡을 수 있다.
 @Primary    // DiscountPolicy 구현 객체로 RateDiscountPolicy에 우선권 부여
 public class RateDiscountPolicy implements DiscountPolicy {
 
