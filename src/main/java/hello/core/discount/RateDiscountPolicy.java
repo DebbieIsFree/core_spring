ada628c0e2d2cf7eec256494cc93e643298407d8
Jiwon <wldnjs980409@naver.com>
2023-08-06 09:12:19 +0900

    Annotation 직접 만들어 사용하기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("mainDiscountPolicy")     // 문자열 오류가 발생하면 컴파일 타임 오류라서 발견할 수가 없다.
-@MainDiscountPolicy         // 그래서 직접 만든 애노테이션 사용, 이때 문자열 오류가 발생하면 잡을 수 있다.
-@Primary    // DiscountPolicy 구현 객체로 RateDiscountPolicy에 우선권 부여
+@MainDiscountPolicy         // 직접 만든 애노테이션 사용, 이때 문자열 오류가 발생하면 잡을 수 있다.
+// @Primary    // DiscountPolicy 구현 객체로 RateDiscountPolicy에 우선권 부여
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
