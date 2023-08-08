ad45918ceea63d213e357d5ca75606f8956ee918
Jiwon <wldnjs980409@naver.com>
2023-08-09 04:14:24 +0900

    웹 스코프 (request scope)

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
-public class LogDemoController {
+public class LogDemoController {    // MyLogger가 잘 작동하는지 확인하는 테스트용 컨트롤러
 
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
-// 3)   private final ObjectProvide-<MyLogger> myLoggerProvider;
 
-    @RequestMapping("log-demo")
+    @RequestMapping("log-demo")      // log-demo라는 요청이 왔을 때
     @ResponseBody
-    public String logDemo(HttpServletRequest request){
+    public String logDemo(HttpServletRequest request){   // 자바 표준 서블릿, 요청 정보 받기
+        // 고객이 어떤 URL로 요청했는지 알 수 있다.
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class LogDemoService {
+@RequiredArgsConstructor    // 생성자 자동 주입
+public class LogDemoService {       // 비즈니스 로직이 있는 서비스 계층에서 로그 출력
 
     private final MyLogger myLogger;
 
