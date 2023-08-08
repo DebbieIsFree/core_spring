fef459aba19e7a68580b4e0bbd6fcc276067b64b
Jiwon <wldnjs980409@naver.com>
2023-08-09 00:15:38 +0900

    웹 스코프 (request scope)

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,38 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")   // HTTP 요청당 하나씩 빈 생성, HTTP 요청이 끝나는 시점에 소멸
+public class MyLogger {     // 로그 출력 클래스
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message){
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    // 요청이 들어왔을 때 (생성 시점)
+    @PostConstruct
+    public void init(){
+        // generate globally unique id
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    // request scope는 종료 메서드가 호출된다. (소멸 시점)
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+}
